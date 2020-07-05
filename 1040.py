3133f34a710a566d00ff014cae2749c8a24c00b5
Olex <olex.yakov97@gmail.com>
2020-07-05 20:13:11 +0100

    init

---
+++ 1040.py
@@ -0,0 +1,19 @@
+from typing import List
+import unittest
+
+
+class Solution:
+    def numMovesStonesII(self, stones: List[int]) -> List[int]:
+        stones.sort()
+        self.used
+        pass
+
+
+if __name__ == "__main__":
+    s = Solution()
+    ans = s.numMovesStonesII([7, 4, 9])
+    print(ans)  # 1 2
+    ans = s.numMovesStonesII([6, 5, 4, 3, 10])
+    print(ans)  # 2 3
+    ans = s.numMovesStonesII([100, 101, 104, 102, 103])
+    print(ans)  # 0 0
